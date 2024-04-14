083e2117f1408da5547315bb49dc04749b3b0242
mahirminhajk <kmahirminhaj@gmail.com>
2024-04-14 17:23:24 +0530

    created mongoose model

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import colors from 'colors'
 
 import { createHandler } from "graphql-http/lib/use/express";
 import { ruruHTML } from "ruru/server";
 
 import schema from './schema/schema.js';
+import { connectDB } from './config/db.js'
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+//* connect to db
+connectDB();
 
 //* graphql
 app.use('/graphql', createHandler({
