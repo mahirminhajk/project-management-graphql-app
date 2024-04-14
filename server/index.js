f570944e130e07d53b48696d56010e6e62924a30
mahirminhajk <kmahirminhaj@gmail.com>
2024-04-14 15:59:38 +0530

    project initialization

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const port = process.env.PORT;
+
+const app = express();
+
+app.listen(port, () => {
+
+    console.log(`Server is running on port ${port}`);
+
+});
