04fcf3d8b9fbb3e372dd0543aaf7ce43f70b8daa
mahirminhajk <kmahirminhaj@gmail.com>
2024-04-14 17:12:57 +0530

    added graphql using sample data

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 
+import { createHandler } from "graphql-http/lib/use/express";
+import { ruruHTML } from "ruru/server";
+
+import schema from './schema/schema.js';
+
 dotenv.config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 const app = express();
+
+//* graphql
+app.use('/graphql', createHandler({
+    schema: schema,
+}));
+
+app.get("/iql", (_req, res) => {
+    res.type("html")
+    res.end(ruruHTML({ endpoint: "/graphql" }))
+})
 
 app.listen(port, () => {
 
